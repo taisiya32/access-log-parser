e059a68e16ca285219c237501591f08aca385160
Taisiya Nesterenko <prostotaya@gmail.com>
2024-10-04 15:35:03 +0300

    Добавлен бесконечный цикл, который запрашивает путь к файлу.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+    public static void main (String[] args){
+        findfile();
     }
+
+    public static void findfile() {
+        int correct_ways_count = 1;
+        while(true) {
+            System.out.println("Укажите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            //C:\Users\prost\Desktop\FileDirectory\file.txt
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists==false) {System.out.println("Файл не существует"); continue;}
+            if (isDirectory==true) {System.out.println(" Указанный путь является путём к папке, а не к файлу"); continue;}
+            if ((fileExists==true)&&(isDirectory==false)) {
+                System.out.println("Путь указан верно. " + "Это файл номер " + correct_ways_count);
+                correct_ways_count++;
+            }
+        }}
+
 }
+
